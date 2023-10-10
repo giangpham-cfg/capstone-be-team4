4db402583c820dc0b9bb4874579f2b9a6c7b0f79
jazz <jazxbx@gmail.com>
2023-10-10 02:43:49 -0700

    fix: seed file

--- prisma/data.js
+++ prisma/data.js
@@ -1,39 +1,3 @@
-export const recipes = [
-  {
-    name: "Recipe 1",
-    ingredients: ["Ingredient 1", "Ingredient 2"],
-    instruction: ["Step 1", "Step 2", "Step 3"],
-    mealTime: "BREAKFAST",
-    cookTime: 30,
-  },
-  {
-    name: "Recipe 2",
-    ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"],
-    instruction: ["Step 1", "Step 2"],
-    mealTime: "LUNCH",
-    cookTime: 45,
-  },
-  {
-    name: "Recipe 3",
-    ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"],
-    instruction: ["Step 1", "Step 2"],
-    mealTime: "DINNER",
-    cookTime: 120,
-  },
-];
-
-export const comments = [
-  {
-    text: "Great recipe!",
-  },
-  {
-    text: "Nauur..",
-  },
-  {
-    text: "I loved it!",
-  },
-];
-
 export const userData = [
   { username: "jazz", password: "123" },
   { username: "ketsarin", password: "123" },
